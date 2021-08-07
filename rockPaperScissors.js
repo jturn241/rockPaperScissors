3dba93ff3241569ecdd5f36cb6c01f580616dcef
jturn241 <jturn241@gmail.com>
2021-08-07 14:17:47 -0400

    rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,29 +1,59 @@
 console.log("You're going down!!");
 
 let computerSelection = "";
+let playerSelection = "";
 
 let computerPlay = function () {
     let rps = ["rock", "paper", "scissors"];
-    let randomRPS = rps[Math.floor(Math.random()*rps.length)];
-    computerSelection = randomRPS;
+    computerSelection = rps[Math.floor(Math.random()*rps.length)];
 }
 
-let playerChoice = prompt("rock, paper or scissors?");
-let playerSelection = playerChoice.toLowerCase();
+let playerPlay = function () {
+    playerSelection = prompt("rock, paper or scissors?").toLowerCase();
+}
 
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound (x, y) {
+    computerPlay();
+    playerPlay();
+    x = playerSelection;
+    y = computerSelection;
     if (x === y) {
-        console.log("try again!")
+        console.log("It's a tie!")
     } else if (x === "rock" && y === "paper") {
         console.log("You lose! paper beats rock")
+        computerScore += 1;
     } else if (x === "rock" && y === "scissors") {
         console.log("You win! rock beats scissors")
+        playerScore += 1;
     } else if (x === "paper" && y === "rock") {
         console.log("You win! paper beats rock")
+        playerScore += 1;
     } else if (x === "paper" && y === "scissors") {
         console.log("You lose! scissors beat paper")
+        computerScore += 1;
     } else if (x === "scissors" && y === "rock") {
         console.log("You lose! rock beats scissors")
+        computerScore += 1;
+    } else if (x === "scissors" && y === "paper") {
+        console.log("You win! rock beats scissors")
+        playerScore += 1;
+    }
+}
+
+function game () {
+    let round = 0;
+    while (round < 5) {
+    playRound();
+    round++;
+    }
+    if (playerScore > computerScore) {
+        console.log("Congrats you've beat the machine!")
+    } else if (playerScore === computerScore) {
+        console.log("It's a tie!")
+    } else {
+        console.log("Sorry you lost, better luck next time")
     }
 }
