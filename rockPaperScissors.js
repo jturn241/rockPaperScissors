e27ed7fb7b97bdb6281ec73e95e4256f648439d7
jturn241 <jturn241@gmail.com>
2021-08-07 10:00:20 -0400

    rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,42 +1,29 @@
 console.log("You're going down!!");
 
-function computerPlay () {
+let computerSelection = "";
+
+let computerPlay = function () {
     let rps = ["rock", "paper", "scissors"];
     let randomRPS = rps[Math.floor(Math.random()*rps.length)];
-    return randomRPS;
+    computerSelection = randomRPS;
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-//let computerSelection;
+let playerChoice = prompt("rock, paper or scissors?");
+let playerSelection = playerChoice.toLowerCase();
 
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+
+function playRound (x, y) {
+    if (x === y) {
         console.log("try again!")
-    }
-    else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("You lose, paper beats rock")
-    }
-    else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You win, rock beats scissors")
-    }
-    else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You win, paper beats rock")
-    }
-    else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log("You lose, scissors beat paper")
-    }
-    else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log("You lose, rock beats scissors")
+    } else if (x === "rock" && y === "paper") {
+        console.log("You lose! paper beats rock")
+    } else if (x === "rock" && y === "scissors") {
+        console.log("You win! rock beats scissors")
+    } else if (x === "paper" && y === "rock") {
+        console.log("You win! paper beats rock")
+    } else if (x === "paper" && y === "scissors") {
+        console.log("You lose! scissors beat paper")
+    } else if (x === "scissors" && y === "rock") {
+        console.log("You lose! rock beats scissors")
     }
 }
-
-
-
-
-
-
-
-/*function singleRound (playerSelection, computerSelection) {
-    let playerSelection = parseInt(prompt("Choose your weapon: rock, paper or scissors "))
-}*/
